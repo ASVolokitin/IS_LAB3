87475625012627b144eec25460a659d8059e848c
Alexandr <volokitin200@bk.ru>
2025-11-25 15:01:56 +0300

    client: re-enabled strict mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -47,7 +47,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
+  <StrictMode>
     <RouterProvider router={router} />
+  </StrictMode>
 );
 
 reportWebVitals();
