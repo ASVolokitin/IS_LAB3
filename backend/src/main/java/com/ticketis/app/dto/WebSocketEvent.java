1887836ada58b51054d00429cfeff9bec95eafe4
Alexandr <volokitin200@bk.ru>
2025-11-30 00:10:07 +0300

    server: replaced timestamp with instant

--- backend/src/main/java/com/ticketis/app/dto/WebSocketEvent.java
+++ backend/src/main/java/com/ticketis/app/dto/WebSocketEvent.java
@@ -1,14 +1,15 @@
 package com.ticketis.app.dto;
 
 import com.ticketis.app.model.enums.WebSocketEventType;
-import java.time.LocalDateTime;
+
+import java.time.Instant;
 import lombok.Data;
 
 @Data
 public class WebSocketEvent {
     private WebSocketEventType eventType;
     private Number entityId;
-    private LocalDateTime timestamp;
+    private Instant timestamp = Instant.now();
 
     public WebSocketEvent(WebSocketEventType eventType, Number entityId) {
         this.eventType = eventType;
