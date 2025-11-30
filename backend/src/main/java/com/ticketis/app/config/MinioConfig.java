2c0e16de75b186fbe9024ad5aa21c6dcf2e771a5
Alexandr <volokitin200@bk.ru>
2025-11-30 05:45:05 +0300

    server: added MInIO config

---
+++ backend/src/main/java/com/ticketis/app/config/MinioConfig.java
@@ -0,0 +1,28 @@
+package com.ticketis.app.config;
+
+import io.minio.MinioClient;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+@Getter
+@Setter
+public class MinioConfig {
+    private String endpoint;
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+    private Integer presignedUrlExpiry;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
